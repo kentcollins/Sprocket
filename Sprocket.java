c4c42bcbff5fe00116b794c0ac8b47c7e0b03986
Kent Collins <1175379+kentcollins@users.noreply.github.com>
2020-07-07 09:04:44 +0100

    randomly assigning links

--- Sprocket.java
+++ Sprocket.java
@@ -1,11 +1,11 @@
 
+import java.util.ArrayList;
+import java.util.Collections;
+
 import processing.core.PApplet;
 
 /***
- * Builds a sketch by extending the PApplet class. In order for this to work,
- * the core Processing libraries must be available on the build path -- these
- * are located in a Java archive called "core.jar" which is distributed as part
- * of the Processing IDE. Look for Processing>Contents>Java>core.jar
+ * Constructs a random grid array of connected cells resembling bicycle chain.
  *
  */
 
@@ -36,18 +36,62 @@ public class Sprocket extends processing.core.PApplet {
 	}
 
 	public void fillGrid() {
+		ArrayList<Pair> toProcess = new ArrayList<>();
 		for (int r = 0; r < grid.length; r++) {
 			for (int c = 0; c < grid[r].length; c++) {
+				int v = r * grid[r].length + c;
 				if (r % 2 == 1 && c % 2 == 1)
 					grid[r][c] = POS;
-				else {
-					grid[r][c] = (r + c) % 2 == 0 ? NEG : NO;
-					if (grid[r][c] == NO && Math.random() < 0.5
-							&& !border(r, c))
-						grid[r][c] = YES;
+				else if (r % 2 == 0 && c % 2 == 0) {
+					grid[r][c] = NEG;
+
+				} else {
+					grid[r][c] = NO;
+					toProcess.add(new Pair(r, c));
 				}
 			}
 		}
+		process(toProcess);
+	}
+
+	private void process(ArrayList<Pair> links) {
+		Collections.shuffle(links);
+		while (links.size() > 0) {
+			Pair p = links.remove(links.size() - 1);
+			if (!border(p.r, p.c)) {
+				Pair[] endpoints = endpoints(p);
+				if (endpoints[0]!=null && numLinks(endpoints[0])<2) {
+					grid[p.r][p.c] = YES;
+				}
+				if (endpoints[1]!=null && numLinks(endpoints[1])<2) {
+					grid[p.r][p.c] = YES;
+				}
+				
+			}
+		}
+
+	}
+	
+	private Pair[] endpoints(Pair p) {
+		// if even row, links are vertical
+		if (p.r%2==0) {
+			Pair above = p.r>0? new Pair(p.r-1, p.c): null;
+			Pair below = p.r<grid.length-1? new Pair(p.r+1, p.c):null;
+			return new Pair[] {above, below};
+		}
+		Pair left = p.c>0? new Pair(p.r, p.c-1):null;
+		Pair right = p.c<grid[0].length-1? new Pair(p.r, p.c+1):null;
+		return new Pair[] {left, right};
+	}
+	
+	private int numLinks(Pair p) {
+		if (border(p.r, p.c)) return -1;
+		int num = 0;
+		if (grid[p.r-1][p.c]==YES) num++;
+		if (grid[p.r+1][p.c]==YES) num++;
+		if (grid[p.r][p.c-1]==YES) num++;
+		if (grid[p.r][p.c+1]==YES) num++;
+		return num;
 	}
 
 	public boolean border(int r, int c) {
@@ -59,6 +103,7 @@ public class Sprocket extends processing.core.PApplet {
 	}
 
 	public void draw() {
+		// rotate(PI/4);
 		translate(dist, dist);
 		for (int r = 0; r < grid.length; r++) {
 			for (int c = 0; c < grid[r].length; c++) {
@@ -78,10 +123,10 @@ public class Sprocket extends processing.core.PApplet {
 		if (grid[r][c] == POS) {
 			fill(255, 255, 0);
 			ellipse(0, 0, dist * sqrt(2), dist * sqrt(2));
-			double quarter = grid.length/4f;
-			if (r < quarter || r > 3*quarter) {
+			double quarter = grid.length / 4f;
+			if (r < quarter || r > 3 * quarter) {
 				fill(64);
-				ellipse(0, 0, dist / 2.0f, dist / 2.0f);
+				ellipse(0, 0, dist / 1.5f, dist / 1.5f);
 			}
 		} else if (grid[r][c] == NEG) {
 			// cover with arcs in units of PI/4 if connection
@@ -136,4 +181,12 @@ public class Sprocket extends processing.core.PApplet {
 	 * See the PApplet documentation to discover other methods that can be
 	 * overridden.
 	 */
+	class Pair {
+		int r, c;
+
+		Pair(int r, int c) {
+			this.r = r;
+			this.c = c;
+		}
+	}
 }
